29c53c18a9f7247aab98e0821bd55a7efb178f62
Zach Tellman <ztellman@gmail.com>
2013-09-03 14:01:52 -0700

    target byte-streams 0.1.5

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Methods for hashing, compressing, and encoding bytes."
   :license {:name "Apache License 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[byte-streams "0.1.5-SNAPSHOT"]
+  :dependencies [[byte-streams "0.1.5"]
                  [net.jpountz.lz4/lz4 "1.1.2"]
                  [org.xerial.snappy/snappy-java "1.1.0-M3"]
                  [commons-codec/commons-codec "1.8"]]
