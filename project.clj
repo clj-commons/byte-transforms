c65f21a2f7ba399fa332cebb7636d000ff8997e0
Erik Assum <erik@assum.net>
2020-12-25 11:02:38 +0100

    Build infra

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject byte-transforms "0.1.4"
+(defproject clj-commons/byte-transforms (or (System/getenv "version") "0.1.4")
   :description "Methods for hashing, compressing, and encoding bytes."
   :license {:name "Apache License 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
@@ -7,10 +7,13 @@
                  [commons-codec/commons-codec "1.10"]
                  [net.jpountz.lz4/lz4 "1.3"]
                  [org.apache.commons/commons-compress "1.18"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0-alpha6"]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]
                                   [criterium "0.4.3"]
-                                  [org.clojure/test.check "0.7.0"]
-                                  [codox-md "0.2.0" :exclusions [org.clojure/clojure]]]}}
+                                  [org.clojure/test.check "1.1.0"]
+                                  [codox-md "0.2.0" :exclusions [org.clojure/clojure]]]}
+             :ci {:javac-options ["-target" "1.8" "-source" "1.8"]
+                  :dependencies [[org.clojure/clojure "1.10.1"]
+                                 [org.clojure/test.check "1.1.0"]]}}
   :plugins [[codox "0.6.4"]]
   :codox {:writer codox-md.writer/write-docs
           :include [byte-transforms]}
